68c3e47c3577707f8d6fa6c6f01229d7281675a2
Vaclav Pavlicek <vaclav@pavlicek.cz>
2015-12-21 14:59:18 +0100

    do next step (divide number with 2 or multiply with 3 and sum 1)

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 def is_even_number(number):
     return number % 2 == 0
+
+
+def do_next_step(number):
+    if is_even_number(number):
+        return number / 2
+    else:
+        return number * 3 + 1
--- test.py
+++ test.py
@@ -6,3 +6,7 @@ class WorkForSaraTest(unittest.TestCase):
     def test_is_even_number(self):
         self.assertEqual(True, is_even_number(2))
         self.assertEqual(False, is_even_number(3))
+
+    def test_do_next_step(self):
+        self.assertEqual(9, do_next_step(18))
+        self.assertEqual(28, do_next_step(9))
